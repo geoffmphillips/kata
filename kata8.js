0ffe4ad4cd565e1b32a91c6f066cee67e50a3e8a
Geoff Phillips <geoffreymartinphillips@gmail.com>
2018-07-31 03:58:35 +0000

    Finished kata8, created kata9

--- kata8.js
+++ kata8.js
@@ -1,5 +1,22 @@
 var prompt = require("prompt-sync")();
 
-// code below (replace this example)
-var answer = prompt("Guess a number:");
-console.log("You answered: " + answer);
+var count = 0;
+var number = Math.floor((Math.random() * 10) + 1);
+
+while (true) {
+  var answer = prompt("Guess a number between 1 and 10: ");
+  count++;
+  if (isNaN(Number(answer))) {
+    console.log("That's not a number");
+    count--;
+  } else if (answer < number) {
+    console.log("Too low! Try again.");
+  }
+  else if (answer > number) {
+    console.log("Too high! Try again.");
+  }
+  else {
+    console.log("Thats correct! You took " + count + " attempts.");
+    break;
+  }
+}
